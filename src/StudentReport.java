4405b22e9dbe947963408cfbba15fb06df164481
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-03-24 22:56:49 -0500

    Corriendo metodo en StudentReport en sprint-1

--- src/StudentReport.java
+++ src/StudentReport.java
@@ -2,4 +2,8 @@ public class StudentReport {
     public void generarReporte() {
         System.out.println("Generando reporte de estudiantes...");
     }
+
+    public void mensajeBranch() {
+        System.out.println("Trabajo en sprint-1");
+    }
 }
